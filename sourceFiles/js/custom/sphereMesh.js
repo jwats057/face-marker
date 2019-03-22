8ef7279cbcbae996fe2aa464b21d40d0aff696cc
narev3 <nicolas.arevaloabc@gmail.com>
2019-03-22 17:24:17 -0400

    Added sphereMesh.js to index.html
    index.html is a rename of faceEditor_F.html; The difference is the simple function call to sphereMesh for creating spheres where the vertices are in the face Mesh. (line 160)

---
+++ sourceFiles/js/custom/sphereMesh.js
@@ -0,0 +1,24 @@
+    //Adds spheres to the vertices of the face object (mesh)
+    //Then push to objects array
+    function sphereMesh(mesh, object){
+      var sphereGeom;
+      var material;
+      var sphere;
+      var sphereMeshes = [];
+
+      //for each vertex, create sphere mesh and translate to face mesh vertices
+      for(var i = 0; i < mesh.geometry.vertices.length; i++){
+        sphereGeom = new THREE.SphereGeometry(.01,7,7);
+        material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        sphere = new THREE.Mesh(sphereGeom, material);
+        sphere.geometry.translate(mesh.geometry.vertices[i].x, mesh.geometry.vertices[i].y, mesh.geometry.vertices[i].z);
+        scene.add(sphere);
+        sphereMeshes.push(sphere);
+      }
+
+	  //push sphere meshes to objects array
+      for(var i = 0; i < mesh.geometry.vertices.length; i++){
+          object[sphereMeshes[i].uuid] = sphereMeshes[i];
+      }
+
+    }
